66dedb7c75dddc3a336ceeac27fa0ad3479a37cc
Thibault Caron <thibault.caron@laplateforme.io>
2025-03-14 19:34:17 +0100

    Feat: update and test main()

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from Models import database
 def main():
     db = database.Database("Store_rtp")
     db.connect()
+    print("Connected to the database")
     # db.populate_categories()
     # db.populate_products()
     db.close()
+
+if __name__ == "__main__":
+    main()
